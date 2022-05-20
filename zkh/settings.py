d860bb6312ccfc9bd0fc8b71bca22ab21a4bb39c
AHAHACUK <mrtiwesyes@gmail.com>
2022-05-20 09:33:19 +0500

    Disabling password validation

--- zkh/settings.py
+++ zkh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v^%e$kc(62byn*_&otf!11s#kj$@g-fupg)4-s)()seew@js-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,18 +104,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
 ]
 
 
