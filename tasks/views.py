c5c2ac5324e3e07b16fc52225a38e7d02388cd47
AHAHACUK <mrtiwesyes@gmail.com>
2022-05-20 09:25:19 +0500

    Viewsets

--- tasks/views.py
+++ tasks/views.py
@@ -14,18 +14,30 @@ class ReportViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
-    queryset = models.Report.objects.all()
     serializer_class = serializers.ReportSerializer
     permission_classes = []
+
+    def get_queryset(self):
+        queryset = models.Report.objects.all()
+        task_id = self.request.query_params.get('task_id')
+        if task_id:
+            queryset = queryset.filter(task__id=task_id)
+        return queryset
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
-    queryset = models.Task.objects.all()
     serializer_class = serializers.TaskSerializer
     permission_classes = []
+
+    def get_queryset(self):
+        queryset = models.Task.objects.all()
+        worker_id = self.request.query_params.get('worker_id')
+        if worker_id:
+            queryset = queryset.filter(assigned_worker__id=worker_id)
+        return queryset
 
 
 def report_image_media_access(request, task, image_index):
